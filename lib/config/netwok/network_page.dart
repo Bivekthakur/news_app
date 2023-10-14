272cdbbe4c8c9dc35bb3ec0d2d5adc007dfb14ec
Bivek Thakur <thakuraashik74@gmail.com>
2023-10-14 20:23:26 +0530

    news app update

--- lib/config/netwok/network_page.dart
+++ lib/config/netwok/network_page.dart
@@ -1,13 +1,24 @@
 import 'package:dio/dio.dart';
+import 'package:newsapp/feature/tech-crunch/technews_model.dart';
 
 class NetworkRequest{
  static final dio = Dio();
 
 static String techNews = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=00f670e2ccb14290af20f5ac6bc4f9f3';
 
- static Future <void> getTechCrunchNews() async {
+ static Future <List<Articles>?> getTechCrunchNews() async {
 
  final response = await dio.get(techNews);
  print ('My Response: ${response}');
+ print('Status Code: ${response.statusCode}');
+
+ if(response.statusCode == 200 || response.statusCode == 201){
+      // Do thing when our request is sucess.
+  return TechnewsModel.fromJson(response.data).articles ?? [];
+
+ }
+
+ return [];
+
  }
 }
--- lib/feature/tech-crunch/techcrunch_news.dart
+++ lib/feature/tech-crunch/techcrunch_news.dart
@@ -11,12 +11,75 @@ class TechCrunchNews extends StatefulWidget {
 class _TechCrunchNewsState extends State<TechCrunchNews> {
   @override
   Widget build(BuildContext context) {
-    NetworkRequest.getTechCrunchNews();
+
     return  Scaffold(
-      body: ListView(
-        children: [
-          Text('Tech news'),
-        ],
+      appBar: AppBar(title:Text('Tech News')),
+      body: RefreshIndicator(
+        onRefresh: () async{
+          NetworkRequest.getTechCrunchNews();
+        },
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+
+            FutureBuilder(
+                future: NetworkRequest.getTechCrunchNews(),
+                builder: (context , snapshot){
+
+                  if(snapshot.hasData){
+
+                    return ListView.separated(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: snapshot.data?.length ?? 0,
+                        itemBuilder: (context , index){
+                        var news = snapshot.data?[index];
+
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Card(
+                              child: Column(
+                                children: [
+                                  Text(news?.title ?? ''),
+                                  Image.network(news?.urlToImage ?? ''),
+                                  Text(news?. description ?? ''),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(news?.author ?? ''),
+                                      Text(news?.publishedAt ?? ''),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+
+                        },
+                      separatorBuilder: (BuildContext context, int index) {
+                         return  Divider();
+                      },
+                    );
+
+
+                  }
+                  else if(snapshot.hasError){
+                     return Center(child: Text('Error: ${snapshot.error}'),);
+
+                  }
+                  else{
+
+                    return const Center(child: CircularProgressIndicator());
+                  }
+
+                }
+            )
+
+          ],
+        ),
       ),
     );
   }
